9be718d6c11c72149eec7cad0967e0d4d3b101df
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-02 15:29:42 -0600

    Initialize Project
    Xcode boilerplate code. Write basic layout for application (no functionality at this point)
    
    Related to: none

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Darren Rambaud on 02/02/2020.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+
+}
